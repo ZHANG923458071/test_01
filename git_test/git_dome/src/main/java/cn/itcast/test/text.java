00fc954427e7ec6c5f52e4b1f0945306f83a7dfd
xiang <zhangxiang_zte@163.com>
2019-06-16 21:09:40 +0800

    修改git_test工程

--- git_test/git_dome/src/main/java/cn/itcast/test/text.java
+++ git_test/git_dome/src/main/java/cn/itcast/test/text.java
@@ -3,5 +3,7 @@ package cn.itcast.test;
 public class text {
     public static void main(String[] args) {
         System.out.println("111111111111");
+        System.out.println(1);
+        System.out.println(2);
     }
 }
